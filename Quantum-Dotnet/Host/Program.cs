e84b25cded2d0784d0204ccd15a5ebb8675ca603
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-07-16 21:05:45 +0530

    Add C# code to call Superposition operation

--- Quantum-Dotnet/Host/Program.cs
+++ Quantum-Dotnet/Host/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using Quantum_Code;
 using Microsoft.Quantum.Simulation.Simulators;
+using Microsoft.Quantum.Simulation.Core;
 
 namespace Host
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
+            // using (var qsim = new QuantumSimulator())
+            // {
+            //     HelloQ.Run(qsim).Wait();
+            // }
+
+            const int LOOP_MAX = 1000;
+            var ones = 0;
             using (var qsim = new QuantumSimulator())
             {
-                HelloQ.Run(qsim).Wait();
+                for (int i = 0; i < LOOP_MAX; i++)
+                {
+                    var result = await Superposition.Run(qsim);
+                    if(result == Result.One)
+                        ones++;
+                }
             }
+
+            System.Console.WriteLine("Collapsed states:");
+            System.Console.WriteLine($"\t   One: {ones}");
+            System.Console.WriteLine($"\t  Zero: {LOOP_MAX - ones}");
         }
     }
 }
