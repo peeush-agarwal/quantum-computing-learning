1b324bb2cd18905992943c2f2a71fd72324339d7
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-07-16 21:44:18 +0530

    Update Entanglement demo to showcase how changing one qubit affects others when both are entangled

--- Quantum-Dotnet/Host/Program.cs
+++ Quantum-Dotnet/Host/Program.cs
@@ -9,12 +9,38 @@ namespace Host
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
+            await Demo_EntanglementAsync(); return;
+
             await Demo_SuperpositionAsync(); return;
 
             HelloWorld(); return;
             
         }
     
+        static async System.Threading.Tasks.Task Demo_EntanglementAsync(){
+            const int LOOP_MAX = 1000;
+            var ones = 0;
+            var equal = 0;
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int i = 0; i < LOOP_MAX; i++)
+                {
+                    var (resultOne, resultTwo) = await Entanglement.Run(qsim);
+
+                    if(resultOne == Result.One)
+                        ones++;
+                    
+                    if(resultOne == resultTwo)
+                        equal++;
+                }
+            }
+
+            System.Console.WriteLine("Entanglement results:");
+            System.Console.WriteLine($"\t   Ones: {ones}");
+            System.Console.WriteLine($"\t  Zeros: {LOOP_MAX - ones}");
+            System.Console.WriteLine($"\t  Equal: {100*equal/LOOP_MAX} %");
+        }
+
         static void HelloWorld(){
             using (var qsim = new QuantumSimulator())
             {
